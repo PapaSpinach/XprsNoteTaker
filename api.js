e5cad0f045dd1317169b5c6295618a98d36ef50a
Aaron Beckles <therealini@gmail.com>
2024-05-04 17:24:38 -0400

    set up creating notes

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs/promises');
+const crypto = require('crypto');
 
 const dbLocation = path.join(__dirname, './db/db.json');
 
@@ -10,9 +11,31 @@ router.get('/notes', async (req, res) => {
   res.send(notes);
 });
 
+router.post('/notes', async (req, res) => {
+  const note = req.body;
+
+  note.id = generateUniqueId();
+
+  const notes = await loadNotes();
+
+  notes.push(note);
+
+  await saveNotes(notes);
+
+  res.send(200);
+});
+
 async function loadNotes() {
   const content = await fs.readFile(dbLocation);
   return JSON.parse(content);
+}
+
+async function saveNotes(notes) {
+  await fs.writeFile(dbLocation, JSON.stringify(notes, null, 2));
+}
+
+function generateUniqueId() {
+  return crypto.randomBytes(16).toString('hex');
 }
 
 module.exports = router;
